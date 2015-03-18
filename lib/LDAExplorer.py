4017548e6e188bd99bbf971ca6e57d7391ed26ab
David Yerrington <david_yerrington@yahoo.com>
2015-03-17 18:45:58 -0700

    Document handling updates.  Database import.  Generic transformations.

--- lib/LDAExplorer.py
+++ lib/LDAExplorer.py
@@ -1,38 +1,61 @@
 #!/Users/davidyerrington/virtualenvs/data/bin/python
 
-import logging, gensim, bz2, os, string, glob, os, numpy as np, pandas as pd
+import logging, gensim, bz2, os, string, glob, os, numpy as np, pandas as pd, sqlite3 as db
 from sklearn.preprocessing import scale
 from gensim import corpora, models, similarities
 from nltk.corpus import stopwords
 from os import path
-
+from operator import itemgetter
+import var_dump
+ 
 class LDAExplorer:
 
-    stopwords_file  =   'stopwords.txt'
+    stopwords_file  =   '../data/stopwords.txt'
     stopwords       =   False
     corpus_files    =   False
-    corpus_data     =   '/tmp/corpus_data.mm'
-    dictionary_data =   '/tmp/dictionary.data'
-    model_file      =   '/tmp/model.lda'
-    logfile         =   'lda_stats.log'
+    corpus_files_meta = '../data/corpus.meta'
+    corpus_data     =   '../data/corpus_data.mm'
+    document_db     =   '../data/docs.db'
+    dictionary_data =   '../data/dictionary.data'
+    model_file      =   '../data/model.lda'
+    logfile         =   'lda_stats.log' 
     logging         =   {
         'filename': 'lda_stats.log',
         'format':   '%(asctime)s : %(levelname)s : %(message)s',
         'level':    logging.INFO
     }
 
-    number_of_topics =  20
+    number_of_topics =  10
 
     def __init__(self):
 
         logging.basicConfig(filename=self.logging['filename'], format=self.logging['format'], level=self.logging['level'])
         self.set_stopwords()
+        
+        try:
+            print "Trying to load corpus files"
+            self.corpus_files = pd.read_pickle(self.corpus_files_meta)
+            print "done.."
+            print self.corpus_files.iloc[4567]
+
+        except:
+            directory = '/var/www/htdocs/rapalytics/data/albums/' # temp location
+            albums  =   [f for f in os.listdir(directory) if os.path.isdir(directory + f)]
+            files   =   [glob.glob(directory + file + '/*.txt') for file in albums]
+            self.corpus_files   =   [item.split('/var/www/htdocs/rapalytics/data/albums/')[1] for files in files for item in files]
+            corpus_files_meta   =   pd.DataFrame(self.corpus_files)
+            corpus_files_meta.save(self.corpus_files_meta)
+
+            print "loading files, saving dataframe ====================================== should not be loading!!!!!!!!!!"
+            print corpus_files_meta.head()
+
+
 
     def set_stopwords(self):
 
         try:
             with open(self.stopwords_file) as fp:
-                self.stopwords = set(fp.read().encode('utf-8').strip().split())
+                self.stopwords = set(fp.read().encode('utf-8').replace("\xe2", "").strip().split())
 
         except IOError as e:
             print "set_stopwords() error: {})".format(e)
@@ -120,17 +143,20 @@ class LDAExplorer:
             # save for later..
             gensim.corpora.MmCorpus.serialize(self.corpus_data, self.corpus)
 
+    # TBD:  move lda to self.lda
     def generate_topics_lda(self):
-
+        print "Generating new topics..." 
         lda     =   gensim.models.ldamodel
         
         try:
             model = lda.LdaModel.load(self.model_file)
         except:
             print "Generating new model..."
-            model = lda.LdaModel(corpus=self.corpus, id2word=self.dictionary, num_topics=self.number_of_topics, update_every=1, chunksize=1000, passes=4)
+            
+            model = lda.LdaModel(corpus=self.corpus, alpha='auto', id2word=self.dictionary)
+            # model = lda.LdaModel(corpus=self.corpus, alpha=.01, id2word=self.dictionary, num_topics=self.number_of_topics, update_every=1, chunksize=1000, passes=4)
             model.save(self.model_file)
-
+            
         scores = []
 
         for idx, topic in enumerate(model.show_topics(num_topics=self.number_of_topics, num_words=25, formatted=False)): # topn=20
@@ -148,14 +174,144 @@ class LDAExplorer:
         # scores = np.array(scores)
         # print scores / float(max(scores)) * 100
 
-        
+    def get_topics(self, num_words=5, labels_only=True, topic_id=False):
 
+        print "Getting topics...."
+
+        lda     =   gensim.models.ldamodel
+        model   =   lda.LdaModel.load(self.model_file)
+        scores  =   {}
+
+        if topic_id:
+
+            topics      =   model.show_topics(num_words=num_words, formatted=False)[int(topic_id)]
+            max_prob    =   max(topics, key=itemgetter(0))[0]
+
+            return ((int(round((p / max_prob) * 100)), topic) for p, topic in topics)
+
+        for idx, topic in enumerate(model.show_topics(num_words=num_words, formatted=False)): # topn=20
+
+            if labels_only:
+                scores.update({
+                    idx: ', '.join([item[1].encode('utf-8').strip() for item in topic])
+                })
+
+            else:
+
+                scores.update({
+                    idx: topic
+                })
+
+        return scores
+    
+    def get_lda_documents(self, topic_id=5, max_results=35):
+        conn    =   db.connect(self.document_db) 
+        sql     =   'SELECT topic_id, document_name, ROUND(score * 100, 0) as score FROM topics WHERE topic_id = %d ORDER BY score DESC LIMIT %d' % (int(topic_id), max_results)
+        topics  =   pd.read_sql(sql, con=conn)
+
+        # print topics.to_dict('records')
+        conn.close()
+        return topics.to_dict('records')
+
+    def get_lda_documents_legacy(self, topic_id=0):
+        print "Getting documents.................."
+        lda     =   gensim.models.ldamodel
+        try:
+            self.corpus =   corpora.MmCorpus(self.corpus_data)
+            model       =   lda.LdaModel.load(self.model_file)
+            corpus_lda  =   model[self.corpus]
+            # sorted(key=lambda item: -item[1])
+            simularity  =   list(enumerate(corpus_lda))
+
+            topics      =   {}
+
+            # print self.corpus_files[:20]
+
+            # Amazing optimization - python rulez #1
+            scores = sorted(corpus_lda, reverse=True, key=lambda doc: abs(dict(doc).get(topic_id, 0.0))) 
+
+
+            # intialize dictionary of topics
+            for n in list(xrange(self.number_of_topics)):
+                topics.update({n: []})
+
+            # append list of documents in topic
+            for (document_id, scores) in simularity[:50]:
+                for topic_id, score in scores:
+                    # self.corpus_files.iloc[document_id][0]
+                    topics[topic_id].append((document_id, (int(round(score * 100)))))
+
+            # sort by score ascending
+            for (topic_id, scores) in topics.items():
+                topics[topic_id] = sorted(topics[topic_id], key=lambda item: item[1], reverse=True)
+            # print model.print_topics(model.num_topics)[topic_id]
+            
+            return topics[topic_id]
+
+            # print "length of corpus_lda: %d, length of documents array: " % (len(corpus_lda))
+            # print self.
+
+        except (RuntimeError, TypeError, NameError):
+            print "get_lda_documents() exception not implemented.. %s" % RuntimeError
+
+    def import_document_topics(self, topic_id=1, max_documents=10):
+        
+        print "Importing document topics to sqlite...."
+
+        lda         =   gensim.models.ldamodel
+        self.corpus =   corpora.MmCorpus(self.corpus_data)
+        model       =   lda.LdaModel.load(self.model_file)
+        corpus_lda  =   model[self.corpus]
+
+        # scores = sorted(corpus_lda, reverse=True, key=lambda doc: abs(dict(doc).get(topic_id, 0.0))) 
+
+        topics = []
+
+        for document_id, row in enumerate(corpus_lda):
+            for topic_id, score in row:
+                topics.append({
+                    'document_id':      document_id, 
+                    'document_name':    self.corpus_files.iloc[document_id][0],
+                    'topic_id':         topic_id, 
+                    'score':            score
+                })
+        
+        conn = db.connect('../data/docs.db') 
+        topics = pd.DataFrame(topics)
+        print topics.head()
+        topics.to_sql('topics', con=conn, if_exists='replace')
+        conn.close() 
+
+        # print "score lenght", len(scores)
+
+        # for score in scores[:40]:
+            # print score
+
+        # print [(self.corpus_files.iloc[score[0]][0], score[0]) for score in enumerate(scores[:max_documents])]
+
+        # print scores[:max_documents]
+
+
+    def get_simularities(self, topic_id=1):
+        lsi = models.LsiModel(self.corpus, id2word=self.dictionary, num_topics=2)
+        vec_lsi = lsi[self.dictionary] # convert the query to LSI space
+        print(vec_lsi) 
+
+        index = similarities.MatrixSimilarity(lsi[self.corpus])
+
+print "Loaded library LDAExplorer............"
 
 lda = LDAExplorer()
+
+# lda.get_lda_documents(topic_id=5)
+
 lda.load_corpus_directory('/var/www/htdocs/rapalytics/data/albums/')
 lda.generate_topics_lda()
+# lda.get_topics()
+# lda.get_lda_documents()
+lda.import_document_topics()
 
 # print lda.stopwords
 
 # print lda.corpus_vectors
-print lda
+# print lda
